ca26c6e9416f75c633d7d1dcd96798d171a7fc59
korelhayrullah <korel_hayrullah@hotmail.com>
2018-02-18 06:13:33 +0300

    started SignupVC and added some functionalities

---
+++ PowerShare/Extensions/SigninVCExt.swift
@@ -0,0 +1,155 @@
+//
+//  SigninVCExt.swift
+//  PowerShare
+//
+//  Created by Korel Hayrullah on 18.02.2018.
+//  Copyright © 2018 PowerShare. All rights reserved.
+//
+
+import UIKit
+
+extension SigninVC{
+    //MARK: TextField Handling
+    
+    @objc func keyboardWillShow(_ notification: Notification) {
+        if let keyboardFrame: NSValue = notification.userInfo?[UIKeyboardFrameEndUserInfoKey] as? NSValue {
+            let keyboardRectangle = keyboardFrame.cgRectValue
+            keyboardHeight = self.view.frame.size.height - keyboardRectangle.height
+            
+            if let activeKeyboard = currentylActiveKeyboard{
+                if activeKeyboard.frame.origin.y > keyboardHeight - emptySpacePt{
+                    UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .curveEaseOut, animations: {
+                        self.view.frame.origin.y -= (activeKeyboard.frame.origin.y - (keyboardHeight - emptySpacePt))
+                    }, completion: nil)
+                }
+            }
+        } else {
+            if let activeKeyboard = currentylActiveKeyboard ,let keybHeight = keyboardHeight{
+                if activeKeyboard.frame.origin.y > keybHeight - emptySpacePt{
+                    UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .curveEaseOut, animations: {
+                        self.view.frame.origin.y -= (activeKeyboard.frame.origin.y - (keybHeight - emptySpacePt))
+                    }, completion: nil)
+                } else {
+                    UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .curveEaseOut, animations: {
+                        self.view.frame.origin.y = 0
+                    }, completion: nil)
+                }
+            }
+        }
+    }
+    
+    @objc func keyboardWillHide(_ notification: Notification) {
+        if view.frame.minY < 0{
+            UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .curveEaseOut, animations: {
+                self.view.frame.origin.y = 0
+            }, completion: nil)
+        }
+    }
+    
+    internal override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        currentylActiveKeyboard = nil
+        view.endEditing(true)
+    }
+    
+    internal func textFieldDidBeginEditing(_ textField: UITextField) {
+        textField.becomeFirstResponder()
+        currentylActiveKeyboard = textField
+        keyboardWillShow(Notification(name: NSNotification.Name.UIKeyboardWillShow))
+        
+        if let characterCount = textField.text?.count {
+            if textField.tag == 1{
+                counterLabel.text = String(40 - characterCount)
+            } else {
+                counterLabel.text = String(20 - characterCount)
+            }
+        }
+    }
+    
+    internal func textFieldDidEndEditing(_ textField: UITextField) {
+        textField.resignFirstResponder()
+    }
+    
+    internal func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if textField.returnKeyType == .continue{
+            if textField.tag == 1{
+                userPasswordTextField.becomeFirstResponder()
+            } else {
+                userReEnteredPasswordTextField.becomeFirstResponder()
+            }
+        } else if textField.returnKeyType == .go {
+            signupButtonPressed(signupButton)
+        }
+        textField.resignFirstResponder()
+        currentylActiveKeyboard = nil
+        return true
+    }
+    
+    internal func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        let newString = NSString(string: textField.text!).replacingCharacters(in: range, with: string)
+        
+        if textField.tag == 1{
+            counterLabel.text = newString.count == 41 ? String(0) : String(40 - newString.count)
+            return newString.count <= 40
+        } else {
+            counterLabel.text = newString.count == 21 ? String(0) : String(20 - newString.count)
+            return newString.count <= 20
+        }
+    }
+    
+    // fetching user data
+    
+    func fetchUser(username: String, completion: @escaping (User, Int) -> ()){
+        let url = URL(string: "https://power-share-hackathon.herokuapp.com/user/info")
+        var request = URLRequest(url: url!)
+        request.setValue("application/x-www-form-urlencoded", forHTTPHeaderField: "Content-Type")
+        request.httpMethod = "POST"
+        
+        let postString = "username=\(username)"
+        
+        request.httpBody = postString.data(using: .utf8)
+        
+        let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+            guard let data = data, error == nil else {
+                print("error=\(String(describing: error))")
+                DispatchQueue.main.async {
+                    //showing an alert if something goes wrong
+                    let alert = UIAlertController(title: "Upsss!", message: "The load has been added to the completion queue. This will be processed once there is a connection.", preferredStyle: .alert)
+                    alert.addAction(UIAlertAction(title: "OK", style: .default))
+                    self.present(alert, animated: true, completion: nil)
+                }
+                return
+            }
+            
+            if let httpStatus = response as? HTTPURLResponse, httpStatus.statusCode != 200 {
+                print("statusCode should be 200, but is \(httpStatus.statusCode)")
+                print("response = \(String(describing: response))")
+            }
+            
+            do{
+                let json = try JSONSerialization.jsonObject(with: data, options: []) as? NSDictionary
+                
+                if let json = json{
+                    DispatchQueue.main.async {
+                        guard let password = json.value(forKey: PASSWORD) as? String else { return }
+                        guard let username = json.value(forKey: NAME) as? String else { return }
+                        guard let totalCredit = json.value(forKey: TOTAL_CREDIT) as? Double else { return }
+                        
+                        user = User(username: username, password: password, totalCredit: totalCredit)
+                        
+                        completion(user!, 1)
+                    }
+                }
+                
+            } catch {
+                print(error)
+                DispatchQueue.main.async {
+                    completion(User(username: EMPTY_STRING, password: EMPTY_STRING, totalCredit: 0), 0)
+                }
+            }
+            
+        }
+        task.resume()
+    }
+
+}
+
--- PowerShare/View Controllers/SigninVC.swift
+++ PowerShare/View Controllers/SigninVC.swift
@@ -8,10 +8,174 @@
 
 import UIKit
 
-class SigninVC: UIViewController {
-
+class SigninVC: UIViewController, UITextFieldDelegate {
+    
+    //MARK: Properties
+    
+    //IBOutlets
+    @IBOutlet weak var logoLabel: UILabel!{
+        didSet{
+            logoLabel.textColor = UIColor.green
+            logoLabel.layer.shadowOpacity = 1
+            logoLabel.layer.shadowRadius = 0
+            logoLabel.layer.shadowOffset = CGSize(width: -2, height: 2)
+            logoLabel.layer.shadowColor = UIColor.lightGray.cgColor
+        }
+    }
+    @IBOutlet weak var userNameTextField: UITextField!{
+        didSet{
+            userNameTextField.placeholder = ENTER_USER_NAME
+            userNameTextField.returnKeyType = .continue
+            userNameTextField.autocorrectionType = .no
+            userNameTextField.autocapitalizationType = .none
+            userNameTextField.keyboardType = .alphabet
+            userNameTextField.textColor = UIColor.green
+            userNameTextField.tintColor = userNameTextField.textColor
+            userNameTextField.tag = 1
+            userNameTextField.addIcon(image: UIImage(named: PROFILE_ICON), leftPadding: 5)
+            userNameTextField.addCharacterCounter(counterLabel: counterLabel, rightPadding: 0)
+        }
+    }
+    @IBOutlet weak var userPasswordTextField: UITextField!{
+        didSet{
+            userPasswordTextField.placeholder = ENTER_PASSWORD
+            userPasswordTextField.isSecureTextEntry = true
+            userPasswordTextField.returnKeyType = .continue
+            userPasswordTextField.autocorrectionType = .no
+            userPasswordTextField.autocapitalizationType = .none
+            userPasswordTextField.keyboardType = .alphabet
+            userPasswordTextField.textColor = UIColor.green
+            userPasswordTextField.tintColor = userPasswordTextField.textColor
+            userPasswordTextField.tag = 2
+            userPasswordTextField.addIcon(image: UIImage(named: PASSWORD_ICON), leftPadding: 5)
+            userPasswordTextField.addCharacterCounter(counterLabel: counterLabel, rightPadding: 0)
+        }
+    }
+    @IBOutlet weak var userReEnteredPasswordTextField: UITextField!{
+        didSet{
+            userReEnteredPasswordTextField.placeholder = ENTER_PASSWORD
+            userReEnteredPasswordTextField.isSecureTextEntry = true
+            userReEnteredPasswordTextField.returnKeyType = .go
+            userReEnteredPasswordTextField.autocorrectionType = .no
+            userReEnteredPasswordTextField.autocapitalizationType = .none
+            userReEnteredPasswordTextField.keyboardType = .alphabet
+            userReEnteredPasswordTextField.textColor = UIColor.green
+            userReEnteredPasswordTextField.tintColor = userReEnteredPasswordTextField.textColor
+            userReEnteredPasswordTextField.tag = 3
+            userReEnteredPasswordTextField.addIcon(image: UIImage(named: PASSWORD_ICON), leftPadding: 5)
+            userReEnteredPasswordTextField.addCharacterCounter(counterLabel: counterLabel, rightPadding: 0)
+        }
+    }
+    @IBOutlet weak var signupButton: UIButton!{
+        didSet{
+            let attributedString =  NSMutableAttributedString(string: "Sign Up", attributes: [NSAttributedStringKey.font: UIFont(name: AVENIR_NEXT_REGULAR, size: 15)!, NSAttributedStringKey.foregroundColor: UIColor.white])
+            signupButton.setAttributedTitle(attributedString, for: .normal)
+            signupButton.backgroundColor = UIColor.green
+            signupButton.layer.shadowColor = UIColor.lightGray.cgColor
+            signupButton.layer.shadowOffset = CGSize(width: 0, height: 2)
+            signupButton.layer.shadowOpacity = 1
+            signupButton.layer.shadowRadius = 2
+            signupButton.layer.cornerRadius = 4
+        }
+    }
+    @IBOutlet weak var alreadyHaveAnAccountButton: UIButton!{
+        didSet{
+            let attributedString1 = NSMutableAttributedString(string: "Already have an account? ", attributes: [NSAttributedStringKey.font: UIFont(name: AVENIR_NEXT_REGULAR, size: 15)!, NSAttributedStringKey.foregroundColor: UIColor.green])
+            
+            let attributedString2 = NSMutableAttributedString(string: "Login.", attributes: [NSAttributedStringKey.font: UIFont(name: AVENIR_NEXT_DEMI_BOLD, size: 15)!, NSAttributedStringKey.foregroundColor: UIColor.green])
+            
+            attributedString1.append(attributedString2)
+            
+            alreadyHaveAnAccountButton.setAttributedTitle(attributedString1, for: .normal)
+            alreadyHaveAnAccountButton.backgroundColor = UIColor.clear
+            alreadyHaveAnAccountButton.titleLabel?.adjustsFontSizeToFitWidth = true
+            alreadyHaveAnAccountButton.titleLabel?.minimumScaleFactor = 10 / UIFont.labelFontSize
+        }
+    }
+    
+    
+    //MARK: Methods
     override func viewDidLoad() {
         super.viewDidLoad()
         UIApplication.shared.statusBarStyle = .default
+        
+        // setting delegates
+        userNameTextField.delegate = self
+        userPasswordTextField.delegate = self
+        userReEnteredPasswordTextField.delegate = self
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        setAlphasForFadingINAnimation()
+        animateLoginScreen()
+        
+        // adding observers
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: NSNotification.Name.UIKeyboardWillShow, object: nil)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(_:)), name: NSNotification.Name.UIKeyboardWillHide, object: nil)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        // remove observers
+        NotificationCenter.default.removeObserver(self, name: NSNotification.Name.UIKeyboardWillShow, object: nil)
+        NotificationCenter.default.removeObserver(self, name: NSNotification.Name.UIKeyboardWillHide, object: nil)
+    }
+    
+    //MARK: Animations
+    
+    private func setAlphasForFadingINAnimation(){
+        logoLabel.alpha = 0
+        userNameTextField.alpha = 0
+        userPasswordTextField.alpha = 0
+        userReEnteredPasswordTextField.alpha = 0
+        signupButton.alpha = 0
+        alreadyHaveAnAccountButton.alpha = 0
+    }
+    
+    private func animateLoginScreen(){
+        UIView.animate(withDuration: 0.5, delay: 0, options: .curveLinear, animations: {
+            self.logoLabel.alpha = 1
+        }, completion: nil)
+        UIView.animate(withDuration: 0.5, delay: 0.3, options: .curveLinear, animations: {
+            self.userNameTextField.alpha = 1
+        }, completion: nil)
+        UIView.animate(withDuration: 0.5, delay: 0.6, options: .curveLinear, animations: {
+            self.userPasswordTextField.alpha = 1
+        }, completion: nil)
+        UIView.animate(withDuration: 0.5, delay: 0.9, options: .curveLinear, animations: {
+            self.userReEnteredPasswordTextField.alpha = 1
+        }, completion: nil)
+        UIView.animate(withDuration: 0.5, delay: 1.2, options: .curveLinear, animations: {
+            self.signupButton.alpha = 1
+        }, completion: nil)
+        UIView.animate(withDuration: 0.5, delay: 1.5, options: .curveLinear, animations: {
+            self.alreadyHaveAnAccountButton.alpha = 1
+        }, completion: nil)
+    }
+    
+    //MARK: Actions
+    
+    @IBAction func signupButtonPressed(_ sender: UIButton) {
+        if userNameTextField.text != EMPTY_STRING && userPasswordTextField.text != EMPTY_STRING && userReEnteredPasswordTextField.text != EMPTY_STRING{
+            if userPasswordTextField.text == userReEnteredPasswordTextField.text{
+                
+                // ok create user
+            } else {
+                alert = CustomAlertController(message: "Upsss", description: "Passwords should match!", buttonTitle: "OK", shouldVibrate: false)
+                alert.show()
+            }
+            
+        } else {
+            alert = CustomAlertController(message: "Upsss", description: "Fields cannot left blank.", buttonTitle: "OK", shouldVibrate: false)
+            alert.show()
+        }
+    }
+    
+    @IBAction func alreadyHaveAnAccountButtonPressed(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    
 }
